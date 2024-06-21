8bf5953cb75804a369bcccb1669ffe83becadd27
Thawne2049 <114900962+Thawne2049@users.noreply.github.com>
2024-06-21 23:07:42 +0530

    Add files via upload

---
+++ Tuple eg.py
@@ -0,0 +1,11 @@
+T1=("Apple","Banana","Cherry")
+print(T1)
+print(len(T1))
+T2=(1,5,7,9,3)
+T3=(True,False,True)
+T4=("Banana",1,8,True)
+print(T3[1])
+print(T4[-1])
+print(T4[1:3])
+if "Cherry" in T1:
+    print("yes, cherry is in the tuple")
